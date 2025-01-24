638771a96051e495ddca5346f59973383930ec76
kg07nurislam <152377115+kg07nurislam@users.noreply.github.com>
2025-01-24 20:13:24 +0600

    Update general.ftg.js

--- for/telegram/general.ftg.js
+++ for/telegram/general.ftg.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener('DOMContentLoaded', function() {
   const header = document.getElementById('footer');
 
@@ -21,3 +22,20 @@ document.addEventListener('DOMContentLoaded', function() {
     </nav>
   `;
 });
+*/
+
+// Категорияларды көрсөтүү үчүн функция
+function showPage(page) {
+  const sections = document.querySelectorAll('.page');
+  const buttons = document.querySelectorAll('.page-btn');
+
+  sections.forEach((section) => {
+    section.style.display = 'none';
+  });
+
+  buttons.forEach((button) => {
+    button.classList.remove('active');
+  });
+
+  document.getElementById(page).style.display = 'block';
+};
